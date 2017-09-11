8543299b13bc903ef834ae73e5376b90844335ec
Valeriy Bezuglyy <valnocorner@gmail.com>
2017-09-11 22:34:35 +0300

    added stub actions

--- Mirage/Stub.swift
+++ Mirage/Stub.swift
@@ -9,11 +9,29 @@
 import Foundation
 
 class Stub {
+    
     let functionName: String
+    var actions: [StubAction]
     
     init(functionName: String) {
         self.functionName = functionName
+        actions = []
     }
     
-    
+    //MARK: result
+    func thenReturn(_ result: Any) -> Stub {
+        let stubAction = StubAction { (_) -> Any? in
+            return result
+        }
+        actions.append(stubAction)
+        return self
+    }
+
+    func thenDo(_ closure: @escaping (_ args: [Any?]) -> Void) -> Stub {
+        let stubAction = StubAction { (args) -> Any? in
+            return closure(args)
+        }
+        actions.append(stubAction)
+        return self
+    }
 }
