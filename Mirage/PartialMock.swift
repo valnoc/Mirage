9efa97ef446781d964eb555ba30a433c7f06e9e1
Valeriy Bezuglyy <valnocorner@gmail.com>
2017-09-13 22:23:27 +0300

    added file

---
+++ Mirage/PartialMock.swift
@@ -0,0 +1,13 @@
+//
+//  PartialMock.swift
+//  MirageExample
+//
+//  Created by Valeriy Bezuglyy on 13/09/2017.
+//  Copyright © 2017 Valeriy Bezuglyy. All rights reserved.
+//
+
+import Foundation
+
+protocol PartialMock: Mock {
+}
+
