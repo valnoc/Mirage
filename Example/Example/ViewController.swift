d9c6bb36b8b916aedadfe84eea39e3157985d668
Valeriy Bezuglyy <valnocorner@gmail.com>
2019-06-10 19:57:26 +0300

    added workspace + new proj

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Valeriy Bezuglyy on 10/06/2019.
+//  Copyright © 2019 Valeriy Bezuglyy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
